4b440f003c491288f71d40b2c539bf428570868d
Hendi Lee <hendilee24@gmail.com>
2020-07-05 11:19:46 +0700

    Updates

---
+++ precache-manifest.ea861ea807b5f51b8037e4d89307d4bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b71213d179be893ea4a90d991317e81",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "57e3c56a3b2e5bb754a9",
+    "url": "/monster-rolodex/static/css/main.67c217a9.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57e3c56a3b2e5bb754a9",
+    "url": "/monster-rolodex/static/js/main.654fbaa9.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
